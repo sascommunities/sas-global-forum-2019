3d02ca77a3133f43526f3ecf56075dee85667c52
Damian Herrick <damian.herrick@sas.com>
2019-04-25 19:01:47 -0400

    Notebook updates, added new packages, and modified the container run script to give it a name.

--- 3601-2019-Herrick/run_container.sh
+++ 3601-2019-Herrick/run_container.sh
@@ -4,6 +4,7 @@ FOLDER=$1
 IMAGE=$2
 
 CONTAINER=`docker run -d --rm -p 8888:8888 \
+           --name viya_$IMAGE \
            -v $FOLDER/notebooks/:/home/ds/notebooks \
            -v $FOLDER/data/:/home/ds/datasets \
            -v $FOLDER/logs/:/home/ds/logs \
